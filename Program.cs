382a44ba4d56ccf2d63af40845f78c95adb41ae5
CJepp <CJepp@DESKTOP-JQH4PRP>
2021-10-15 11:42:53 +0100

    method now iterates to 300. number is now alongside fizz, buzz, fizzbuzz

--- Program.cs
+++ Program.cs
@@ -10,17 +10,20 @@ namespace fizzbuzz_problem
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 101; i++) {   // for loop iterates from 1 - 100 //
+            for (int i = 1; i < 301; i++) {   // for loop iterates from 1 - 100 //
                 if (i % 3 == 0 && i % 5 ==0)  // IF, after dividing i by 3, the remainder is 0 AND the remainder is 0 after dividing i by 5 PRINT "fizzbuzz" //
                 {
-                    Console.WriteLine("Fizzbuzz");
-                } else if (i % 3 == 0)
+                    Console.WriteLine(i + "-Fizzbuzz");
+                } 
+                else if (i % 3 == 0)
                 {
-                    Console.WriteLine("Fizz");
-                }else if (i % 5 == 0)
+                    Console.WriteLine(i + "-Fizz");
+                }
+                else if (i % 5 == 0)
                 {
-                    Console.WriteLine("Buzz");
-                }else
+                    Console.WriteLine(i + "-Buzz");
+                }
+                else
                 {
                     Console.WriteLine(i);
                 }
