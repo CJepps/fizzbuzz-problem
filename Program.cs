a3f0d9985fc4c84b4e4bdcc9e3f6fd236cb7c38a
CJepp <CJepp@DESKTOP-JQH4PRP>
2021-10-15 10:44:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++) {   // for loop iterates from 1 - 100 //
+                if (i % 3 == 0 && i % 5 ==0)  // IF, after dividing i by 3, the remainder is 0 AND the remainder is 0 after dividing i by 5 PRINT "fizzbuzz" //
+                {
+                    Console.WriteLine("Fizzbuzz");
+                } else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
