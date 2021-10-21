8757e817fff4973db03934d6b965a8834938fd19
CJepp <CJepp@DESKTOP-JQH4PRP>
2021-10-21 09:17:30 +0100

    Refactored the previous Fizzbuzz solution, separated main and prime function methods

--- Program.cs
+++ Program.cs
@@ -10,47 +10,55 @@ namespace fizzbuzz_problem
     {
         static void Main(string[] args)
         {
-            bool prime = true;
-            for (int i = 1; i <= 300; i++) // iterates through 1 - 300
-            {
-                for (int j = 2; j <= 300; j++)// divides a by every number between 2 and 300 
+            void main()
+             {
+                for (int i = 1; i <= 300; i++)
                 {
-                    if (i != j && i % j == 0) // if a is not the same as b and the remainder when you divide a/b is not zero, the number cannot be prime
+                    string result = "";
+                    if (primeMethod())
                     {
-                        prime = false;
+                        result += "BuzzFizz";
                         break;
                     }
-                }
-                if (!prime)
-                {
-                    if (i % 3 == 0 && i % 5 == 0)  // IF, after dividing i by 3, the remainder is 0 AND the remainder is 0 after dividing i by 5 PRINT "fizzbuzz" //
-                    {
-                        Console.WriteLine(i + "-Fizzbuzz /r/n");
-                    }
-                    else if (i % 3 == 0)
-                    {
-                        Console.WriteLine(i + "-Fizz /r/n");
-                    }
-                    else if (i % 5 == 0)
-                    {
-                        Console.WriteLine(i + "-Buzz /r/n");
-                    }
-                    else
-                    {
-                        Console.WriteLine(i);
-                    }
-                }
-                else if (prime)
-                {
-                    Console.Write(i + "-Buzzfizz /r/n "); //prints prime number 
 
+                    if (i % 3 == 0)
+                    {
+                        result += "fizz";
+                    }
+                    if (i % 5 == 0)
+                    {
+                        result += "Buzz";
+                    }
+                    Console.WriteLine(i + " " + result);
                 }
-                prime = true;
+             }
+
+             bool primeMethod()
+            {
+                bool prime = true;
+                for (int i = 1; i <= 300; i++) // iterates through 1 - 300
                 
-            
-
+                    for (int j = 2; j <= 300; j++)// divides i by every number between 2 and 300 
+                    {
+                        if (i != j && i % j == 0) // if i is not the same as j and the remainder when you divide i/j is not zero, the number cannot be prime
+                        {
+                            prime = false;
+                            break;
+                        }
+                        
+                    }
+                
+                return prime;
             }
+
+            
+                
+              
+            main();
+            primeMethod();
+
            
+
 
         }
     }
